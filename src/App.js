ca5faf2c3a9265b56030b7d31256ca7a07c4ccf8
Fahad Mahmood <fahad492@outlook.com>
2019-03-15 13:57:45 +0500

    Update the initial repository with random quotes code.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import quotes from './quotes';
+var _ = require('lodash');
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quotes: quotes,
+      index: this.randomIndex()
+    };
+     this.randomIndex = this.randomIndex.bind(this);
+     this.clickhandler = this.clickhandler.bind(this);
+  }
+    
+  randomIndex(){
+     let index = _.random(0,16);
+     console.log(index);
+     return index;
+  }
+  clickhandler() {
+     let val = this.randomIndex();
+      this.setState({
+        index:val
+      })
+    
+  }
+  
   render() {
+    const quote = this.state.quotes[this.state.index];
+    console.log(quote);
+    const url = `https://twitter.com/intent/tweet?text=${quote.quoteText}-${quote.quoteAuthor}`;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+       <div id="quote-box"> 
+        <p id="text">{quote.quoteText}</p>
+        <p id="author">{quote.quoteAuthor}</p>
+        <button id="new-quote" onClick={this.clickhandler}> 
+        New Quote
+        </button>
+        <button>
+          <a id="tweet-quote" href={url} rel="noopener noreferrer" target="_blank">
+            Tweet Quote</a>
+        </button>
+        </div> 
     );
   }
 }
